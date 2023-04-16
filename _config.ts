1e08a0295ce2b575956d55db0ff669907413dc22
masataka <m@clipcrow.com>
2023-04-16 23:43:53 +0900

    esbuild

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
 import sass from "lume/plugins/sass.ts";
+import esbuild from "lume/plugins/esbuild.ts";
 
 const site = lume({
   src: "src",
@@ -11,6 +12,7 @@ const site = lume({
 
 site.use(jsx());
 site.use(sass());
+site.use(esbuild());
 
 site.copy("favicon.ico");
 site.copy("images");
