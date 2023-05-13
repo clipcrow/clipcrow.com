1642957717feef9503853dca81c8aeca974f2c0c
masataka <m@clipcrow.com>
2023-05-13 19:37:56 +0900

    fmt

--- lib/EWwareBrandsign.tsx
+++ lib/EWwareBrandsign.tsx
@@ -2,7 +2,9 @@ export default () => (
   <header>
     <div className="header-wrapper">
       <div className="header_title">
-        <a href="/"><span>ClipCrow</span>Essential Workware</a>
+        <a href="/">
+          <span>ClipCrow</span>Essential Workware
+        </a>
       </div>
     </div>
   </header>
--- lib/EWwareContentsWrapper.tsx
+++ lib/EWwareContentsWrapper.tsx
@@ -1,11 +1,10 @@
-
 // deno-lint-ignore no-explicit-any
 export default (props: { children: any }) => (
-    <section className="maincont contarea">
-        <div className="contents_text">
-            <div className="innerblock info">
-                {props.children}
-            </div>
-        </div>
-    </section>
+  <section className="maincont contarea">
+    <div className="contents_text">
+      <div className="innerblock info">
+        {props.children}
+      </div>
+    </div>
+  </section>
 );
