965bc9a8dd37ff8b8fc14bb2711acff5f7d10afd
masataka <m@clipcrow.com>
2023-05-17 00:07:23 +0900

    fixed

--- src/_components/EWwareBlogNavigation.tsx
+++ src/_components/EWwareBlogNavigation.tsx
@@ -3,14 +3,13 @@ import { Search } from "lume/plugins/search.ts";
 
 export default (props: { page: Page | Data, search: Search }) => {
   if (props.page.data.url) {
-    const previous = props.search.previousPage(props.page.data.url, "blog");
-    const next = props.search.nextPage(props.page.data.url, "blog");
+    const previous = props.search.previousPage(props.page.data.url, "blog", "date=desc");
+    const next = props.search.nextPage(props.page.data.url, "blog", "date=desc");
     return (
       <nav>
         <ul class="pagination justify-content-center">
-          <li class={`page-item${previous ? "" : " disabled"}`}><a class="page-link" href={previous?.data.url}>{previous?.data.titlel}</a></li>
-          <li class="page-item active"><a class="page-link" href={props.page.data.url}>{props.page.data.title}</a></li>
-          <li class={`page-item${next ? "" : " disabled"}`}><a class="page-link" href={next?.data.url}>{next?.data.title}</a></li>
+          { previous ? <li class="page-item"><a class="page-link" href={previous.data.url}> &lt; {previous.data.title} </a></li> : null }
+          { next ? <li class="page-item"><a class="page-link" href={next.data.url}> {next.data.title} &gt; </a></li> : null }
         </ul>
       </nav>
     )
