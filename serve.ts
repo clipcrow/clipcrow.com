62f01436c041a0d1514fc2899823e0c3b7c3db84
masataka <m@clipcrow.com>
2023-08-21 22:22:36 +0900

    404対応

--- serve.ts
+++ serve.ts
@@ -5,6 +5,14 @@ const server = new Server({
   root: `${Deno.cwd()}/_site`,
 });
 
+server.use(async (request, next) => {
+  const response = await next(request);
+  if (response.status === 404) {
+    return Response.redirect("/404/, 404");
+  }
+  return response;
+});
+
 server.start();
 
 console.log("Listening on http://localhost:8000");
