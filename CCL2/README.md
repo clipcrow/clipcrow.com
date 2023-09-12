1b14e9492bc5775088414978d3eef2bcc78a0798
masataka <m@clipcrow.com>
2023-09-12 20:52:31 +0900

    className -> class

--- CCL2/README.md
+++ CCL2/README.md
@@ -3,33 +3,43 @@
 動作方法
 
 - ローカル環境にプルした後、CCL2フォルダに移動して、npm installを実行
-- CCL2/.envファイルを用意して、VITE_APP_SLACK_WEBHOOK_URL キーにSlackのWebhook URLを記載する
+- CCL2/.envファイルを用意して、VITE_APP_SLACK_WEBHOOK_URL キーにSlackのWebhook
+  URLを記載する
 - npm run dev でローカル実行できる
 
 ## React + TypeScript + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This template provides a minimal setup to get React working in Vite with HMR and
+some ESLint rules.
 
 Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md)
+  uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc)
+  uses [SWC](https://swc.rs/) for Fast Refresh
 
 ## Expanding the ESLint configuration
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+If you are developing a production application, we recommend updating the
+configuration to enable type aware lint rules:
 
 - Configure the top-level `parserOptions` property like this:
 
 ```js
-   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-   },
+parserOptions: {
+ ecmaVersion: 'latest',
+ sourceType: 'module',
+ project: ['./tsconfig.json', './tsconfig.node.json'],
+ tsconfigRootDir: __dirname,
+},
 ```
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
+- Replace `plugin:@typescript-eslint/recommended` to
+  `plugin:@typescript-eslint/recommended-type-checked` or
+  `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+- Install
+  [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and
+  add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends`
+  list
--- CCL2/src/App.tsx
+++ CCL2/src/App.tsx
@@ -36,18 +36,17 @@ const App = () => {
     if (formData) {
       const webhookURL = import.meta.env.VITE_APP_SLACK_WEBHOOK_URL;
       try {
-        await fetch( webhookURL,
-          {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              text: `お問い合わせ:\nお名前:  ${formData.name}\nメール:  ${formData.email}\nご用件:  ${formData.business}\n内容:  ${formData.content}`,
-            }),
-          }
-        );
+        await fetch(webhookURL, {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            text:
+              `お問い合わせ:\nお名前:  ${formData.name}\nメール:  ${formData.email}\nご用件:  ${formData.business}\n内容:  ${formData.content}`,
+          }),
+        });
         setIsSubmitted(true);
       } catch (error) {
         console.error("Error sending form data to Slack:", error);
@@ -61,171 +60,170 @@ const App = () => {
       <section className="content__page-title">
         <h1>お問い合わせ</h1>
       </section>
-      {isSubmitted ? (
-        <FormComplete />
-      ) : (
+      {isSubmitted ? <FormComplete /> : (
         <section className="content__maincont">
-          {isConfirmed ? (
-            <FormConfirm
-              Back={handleEdit}
-              Confirm={handleConfirm}
-              data={formData}
-            />
-          ) : (
-            <>
-              <p className="content__form-title">
-                土日祝を除く3営業日以内に担当者よりご連絡させていただきます。お問い合わせ内容により、ご回答までにお時間をいただく場合または、ご回答が出来ない場合がございます。
-              </p>
-              <form className="container" onSubmit={handleSubmit(handleSave)}>
-                <div className="input__box">
-                  <input
-                    className={
-                      errors.name ? "input__text input-error" : "input__text"
-                    }
-                    type="text"
-                    placeholder=" "
-                    {...register("name", {
-                      required: {
-                        value: true,
-                        message: "お名前を入力してください",
-                      },
-                      maxLength: {
-                        value: 30,
-                        message: "お名前は30文字以内で入力してください",
-                      },
-                    })}
-                  />
-                  <label
-                    className={
-                      errors.name ? "input__label label-error" : "input__label"
-                    }
-                  >
-                    お名前 *
-                  </label>
-                  <div className="error-message">{errors.name?.message}</div>
-                </div>
-                <div className="input__box">
-                  <input
-                    className={
-                      errors.email ? "input__text input-error" : "input__text"
-                    }
-                    type="text"
-                    placeholder=" "
-                    {...register("email", {
-                      required: {
-                        value: true,
-                        message: "メールアドレスを入力してください",
-                      },
-                      pattern: {
-                        value: /^\S+@\S+$/i,
-                        message: "有効なメールアドレスではありません",
-                      },
-                      maxLength: {
-                        value: 254,
-                        message:
-                          "メールアドレスは254文字以内で入力してください",
-                      },
-                    })}
-                  />
-                  <label
-                    className={
-                      errors.email ? "input__label label-error" : "input__label"
-                    }
-                  >
-                    メールアドレス *
-                  </label>
-                  <div className="error-message">{errors.email?.message}</div>
-                </div>
-                <div className="radio__wrapper">
-                  <label className="radio__title">ご用件 *</label>
-                  <div className="radio__section">
+          {isConfirmed
+            ? (
+              <FormConfirm
+                Back={handleEdit}
+                Confirm={handleConfirm}
+                data={formData}
+              />
+            )
+            : (
+              <>
+                <p className="content__form-title">
+                  土日祝を除く3営業日以内に担当者よりご連絡させていただきます。お問い合わせ内容により、ご回答までにお時間をいただく場合または、ご回答が出来ない場合がございます。
+                </p>
+                <form className="container" onSubmit={handleSubmit(handleSave)}>
+                  <div className="input__box">
                     <input
-                      type="radio"
-                      id="dev"
-                      value="開発案件プロデュースのご依頼・ご相談"
-                      {...register("business", {
+                      className={errors.name
+                        ? "input__text input-error"
+                        : "input__text"}
+                      type="text"
+                      placeholder=" "
+                      {...register("name", {
                         required: {
                           value: true,
-                          message: "ご用件を選択してください",
+                          message: "お名前を入力してください",
+                        },
+                        maxLength: {
+                          value: 30,
+                          message: "お名前は30文字以内で入力してください",
                         },
                       })}
                     />
-                    <label>開発案件プロデュースのご依頼・ご相談</label>
-                  </div>
-                  <div className="radio__section">
-                    <input
-                      type="radio"
-                      id="info"
-                      value="Essential Workwareについて"
-                      {...register("business", {
-                        required: {
-                          value: true,
-                          message: "ご用件を選択してください",
-                        },
-                      })}
-                    />
-                    <label>Essential Workwareについて</label>
-                  </div>
-                  <div className="radio__section">
-                    <input
-                      type="radio"
-                      id="other"
-                      value="その他"
-                      {...register("business", {
-                        required: {
-                          value: true,
-                          message: "ご用件を選択してください",
-                        },
-                      })}
-                    />
-                    <label>その他</label>
-                  </div>
-                  <div className="error-message">
-                    {errors.business?.message}
-                  </div>
-                </div>
-                <div className="input__box">
-                  <textarea
-                    className={
-                      errors.content
-                        ? "input__textarea input-error"
-                        : "input__textarea"
-                    }
-                    placeholder=""
-                    id="text"
-                    cols={30}
-                    rows={8}
-                    {...register("content", {
-                      required: {
-                        value: true,
-                        message: "お問い合わせ内容を入力してください",
-                      },
-                      maxLength: {
-                        value: 300,
-                        message:
-                          "お問い合わせ内容は300文字以内で入力してください",
-                      },
-                    })}
-                  ></textarea>
-                  <label
-                    className={
-                      errors.content
+                    <label
+                      className={errors.name
                         ? "input__label label-error"
-                        : "input__label"
-                    }
-                  >
-                    お問い合わせ内容 *
-                  </label>
-                  <div className="error-message">{errors.content?.message}</div>
-                </div>
-                <input
-                  className="submit__button"
-                  type="submit"
-                  value="内容確認"
-                />
-              </form>
-            </>
-          )}
+                        : "input__label"}
+                    >
+                      お名前 *
+                    </label>
+                    <div className="error-message">{errors.name?.message}</div>
+                  </div>
+                  <div className="input__box">
+                    <input
+                      className={errors.email
+                        ? "input__text input-error"
+                        : "input__text"}
+                      type="text"
+                      placeholder=" "
+                      {...register("email", {
+                        required: {
+                          value: true,
+                          message: "メールアドレスを入力してください",
+                        },
+                        pattern: {
+                          value: /^\S+@\S+$/i,
+                          message: "有効なメールアドレスではありません",
+                        },
+                        maxLength: {
+                          value: 254,
+                          message:
+                            "メールアドレスは254文字以内で入力してください",
+                        },
+                      })}
+                    />
+                    <label
+                      className={errors.email
+                        ? "input__label label-error"
+                        : "input__label"}
+                    >
+                      メールアドレス *
+                    </label>
+                    <div className="error-message">{errors.email?.message}</div>
+                  </div>
+                  <div className="radio__wrapper">
+                    <label className="radio__title">ご用件 *</label>
+                    <div className="radio__section">
+                      <input
+                        type="radio"
+                        id="dev"
+                        value="開発案件プロデュースのご依頼・ご相談"
+                        {...register("business", {
+                          required: {
+                            value: true,
+                            message: "ご用件を選択してください",
+                          },
+                        })}
+                      />
+                      <label>開発案件プロデュースのご依頼・ご相談</label>
+                    </div>
+                    <div className="radio__section">
+                      <input
+                        type="radio"
+                        id="info"
+                        value="Essential Workwareについて"
+                        {...register("business", {
+                          required: {
+                            value: true,
+                            message: "ご用件を選択してください",
+                          },
+                        })}
+                      />
+                      <label>Essential Workwareについて</label>
+                    </div>
+                    <div className="radio__section">
+                      <input
+                        type="radio"
+                        id="other"
+                        value="その他"
+                        {...register("business", {
+                          required: {
+                            value: true,
+                            message: "ご用件を選択してください",
+                          },
+                        })}
+                      />
+                      <label>その他</label>
+                    </div>
+                    <div className="error-message">
+                      {errors.business?.message}
+                    </div>
+                  </div>
+                  <div className="input__box">
+                    <textarea
+                      className={errors.content
+                        ? "input__textarea input-error"
+                        : "input__textarea"}
+                      placeholder=""
+                      id="text"
+                      cols={30}
+                      rows={8}
+                      {...register("content", {
+                        required: {
+                          value: true,
+                          message: "お問い合わせ内容を入力してください",
+                        },
+                        maxLength: {
+                          value: 300,
+                          message:
+                            "お問い合わせ内容は300文字以内で入力してください",
+                        },
+                      })}
+                    >
+                    </textarea>
+                    <label
+                      className={errors.content
+                        ? "input__label label-error"
+                        : "input__label"}
+                    >
+                      お問い合わせ内容 *
+                    </label>
+                    <div className="error-message">
+                      {errors.content?.message}
+                    </div>
+                  </div>
+                  <input
+                    className="submit__button"
+                    type="submit"
+                    value="内容確認"
+                  />
+                </form>
+              </>
+            )}
         </section>
       )}
     </main>
