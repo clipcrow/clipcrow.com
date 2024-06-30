6620f91e6cb4461e11192f42b511cea62a0562ef
masataka <m@clipcrow.com>
2024-06-30 12:20:08 +0900

    文言微修正

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ export default () => (
       <article className="topcontents">
         <div className="topcopy animated deley01" data-scroll="once toggle(.fadeIn, .invisible) 0,0">
           <h2>システムを磨き、<br />健康で楽しく豊かに。</h2>
-          <p>医療をはじめとする生活必需サービス産業において<br />
-            高付加価値ITサービスによってデータに基づく業務最適化を推進し、<br />
+          <p>生活必需産業全般からその他産業向けまで幅広く<br />
+            高付加価値ITサービスによるデータに基づいた業務最適化を推進し、<br />
             組織的な健康管理の実現と関わる皆さまのQuality of Life向上を目指す。</p>
         </div>
         <div className="toppg animated deley03" data-scroll="once toggle(.fadeIn, .invisible) 0,0">
