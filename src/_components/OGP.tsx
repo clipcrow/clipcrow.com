077ac51a6d8cc9cab53fad6da7e35f310ae77763
masataka <m@clipcrow.com>
2024-07-01 18:34:11 +0900

    OGPイメージを変更

--- src/_components/OGP.tsx
+++ src/_components/OGP.tsx
@@ -4,9 +4,9 @@ export default (props: { title?: string, url?: string, image?: string, descripti
     <meta name="Author" content="ClipCrow LLC." />
     <meta name="description" content={props.description?.replaceAll(/[\n\r]/g, "")} />
     <meta property="og:site_name" content="クリップクロウ合同会社" />
-    <meta property="og:title" content={props.title} />
+    <meta property="og:title" content={props.title || "クリップクロウ合同会社"} />
     <meta property="og:url" content={props.url || "https://www.clipcrow.com/"} />
-    <meta property="og:image" content={props.image || "/product/product_kv.png"} />
+    <meta property="og:image" content={props.image || ""} />
     <meta property="og:description" content={props.description?.replaceAll(/[\n\r]/g, "")} />
     <meta property="og:type" content="website" />
   </>
